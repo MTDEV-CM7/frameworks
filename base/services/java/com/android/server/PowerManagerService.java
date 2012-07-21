d8b79942b7e46024d342eff05957dd6cb51a3996
Joey <joey@joeytroy.com>
2012-07-21 10:50:44 -0600

    Fixed light sensor to be instant when Automatic Brightness is checked in Settings < Display.

--- base/services/java/com/android/server/PowerManagerService.java
+++ base/services/java/com/android/server/PowerManagerService.java
@@ -110,7 +110,7 @@ class PowerManagerService extends IPowerManager.Stub
     private static final int LONG_DIM_TIME = 7000;              // t+N-5 sec
 
     // How long to wait to debounce light sensor changes.
-    private static final int LIGHT_SENSOR_DELAY = 2000;
+    private static final int LIGHT_SENSOR_DELAY = 0001;
     private static final int PROXIMITY_SENSOR_DELAY = 1000;
 
     // trigger proximity if distance is less than 5 cm
